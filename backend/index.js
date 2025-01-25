63b12d97f940f52fe84f6412f279a50f4702208b
pradeep-kumavat <pradeepkumavat108@gmail.com>
2025-01-25 23:54:48 +0530

    some changes

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 //const express = require('express');  //method1
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 import connectDB from "./config/database.js";
 import userRoute from "./routes/userRoute.js";
 import messageRoute from "./routes/messageRoute.js";
@@ -11,6 +12,8 @@ dotenv.config({});
 
 //const app = express();
 const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
 
 //middleware
 app.use(express.urlencoded({extended:true}));
@@ -34,6 +37,13 @@ app.use("/api/v1/message", messageRoute);
 //     connectDB();
 //    console.log(`Server Running at port ${PORT}`);
 // });
+
+
+app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(PORT, ()=>{
     connectDB();
