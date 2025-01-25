c673a394143195df605ffb087c0549418f4e21a7
pradeep-kumavat <pradeepkumavat108@gmail.com>
2025-01-26 00:15:07 +0530

    some changes

--- backend/index.js
+++ backend/index.js
@@ -42,7 +42,7 @@ app.use("/api/v1/message", messageRoute);
 app.use(express.static(path.join(__dirname, "/frontend/build")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+  res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
 });
 
 server.listen(PORT, ()=>{
